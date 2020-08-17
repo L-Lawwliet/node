74c97a11ce9a4141485f7756a69d3934ac1594b5
Lawliet <794331482@qq.com>
2020-08-17 21:43:35 +0800

    修改为animation

--- test_web.js
+++ test_web.js
@@ -2,20 +2,21 @@ const db = require('./dbhelper');
 let express = require('express');
 let app = express();
 var bodyParser = require('body-parser');
-const common = require('./common');
+const common = require('./utils/common');
+const moment = require('./utils/moment');
 
 //url解析 以键值对的形式
 app.use(bodyParser.urlencoded({extended:true}));//Context-Type 为application/x-www-form-urlencoded 时 返回的对象是一个键值对，当extended为false的时候，键值对中的值就为'String'或'Array'形式，为true的时候，则可为任何数据类型。
 app.use(bodyParser.json());//用于解析json 会自动选择最为适宜的解析方式
 // 查询
 app.post('/select', function(req, res) {
-    var sql = "SELECT * FROM user_t";
+    var sql = "SELECT * FROM animation_t";
     var reqBody = req.body;
     var keyword = reqBody.keyword.toString();
     if (keyword) {
         // 模糊查询两种方法直接在SQL语句后加 mysql.escape("%"+req.body.name+"%")
         // sql += " WHERE product_name LIKE "+mysql.escape("%"+req.body.name+"%")
-        sql += " WHERE userName LIKE ?"
+        sql += " WHERE name LIKE ?"
      }
     db.query(sql, ['%' + keyword + '%'], function(result, fields) {
         var resultData = {
@@ -30,12 +31,14 @@ app.post('/select', function(req, res) {
 app.post('/add', function(req,res) {
     var reqBody = req.body;
 	var time = common.getNowDate();//这里有个获取时间的函数 但是是以字符段存储的
-	reqBody.time = time;
-	var addSql = 'INSERT INTO user_t(id,userName,age,createTime) VALUES(0,?,?,?)';
+	var addSql = 'INSERT INTO animation_t(id,name,synopsis,createDate,isEnd,coverImg,updateTime) VALUES(0,?,?,?,?,?)';
 	var addSqlParams = [];
-	addSqlParams[0] = reqBody.userName.toString();
-    addSqlParams[1] = reqBody.age.toString();
-    addSqlParams[2] = time;
+	addSqlParams[0] = reqBody.name.toString();
+    addSqlParams[1] = reqBody.synopsis.toString();
+    addSqlParams[2] = moment(reqBody.createDate).format('YYYY-MM-DD');
+    addSqlParams[3] = reqBody.isEnd;
+    addSqlParams[4] = reqBody.coverImg;
+    addSqlParams[5] = time;
 	db.query(addSql, addSqlParams, function (err, result) {
         // if (err) {
         //     console.log('[INSERT ERROR] - ', err);
@@ -44,7 +47,7 @@ app.post('/add', function(req,res) {
         // }
 		var returnObj = {
             status: 200,
-            message: 'success',
+            message: '添加成功',
 		}
 		res.json(returnObj);
         res.end();
@@ -54,16 +57,19 @@ app.post('/add', function(req,res) {
 app.post('/update', function(req,res) {
     var reqBody = req.body;
 	var time = common.getNowDate();//这里有个获取时间的函数 但是是以字符段存储的
-	var sql = 'UPDATE user_t SET userName = ?,age = ?,updateTime=? WHERE id = ?';
+	var sql = 'UPDATE animation_t SET name=?,synopsis=?,createDate=?,isEnd=?,coverImg=?,updateTime=? WHERE id=?';
 	var params = [];
-	params[0] = reqBody.userName.toString();
-    params[1] = reqBody.age.toString();
-    params[2] = time;
-    params[3] = reqBody.id.toString();
+	params[0] = reqBody.name.toString();
+    params[1] = reqBody.synopsis.toString();
+    params[2] = moment(reqBody.createDate).format('YYYY-MM-DD');
+    params[3] = reqBody.isEnd;
+    params[4] = reqBody.coverImg;
+    params[5] = time;
+    params[6] = reqBody.id.toString();
 	db.query(sql, params, function (err, result) {
 		var returnObj = {
             status: 200,
-            message: 'success',
+            message: '修改成功',
 		}
 		res.json(returnObj);
         res.end();
@@ -76,7 +82,7 @@ app.post('/delete', function(req, res) {
 	reqBody.time = time;
 	var addSqlParams = [];
 	addSqlParams[0] = reqBody.id.toString();
-    db.query('DELETE FROM user_t where id=?', addSqlParams, function (error, results, fields) {
+    db.query('DELETE FROM animation_t where id=?', addSqlParams, function (error, results, fields) {
         var returnObj = {
 			status: 200,
             message: 'success',
